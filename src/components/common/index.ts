5237666e86c8340d2d98528abfe144704d8ebfc2
annop kongdeedai <mooliveaa@gmail.com>
2020-02-29 18:11:38 +0700

    create Row component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
-export { Text, FormDisplay, FormDisplayChildren } from "./text";
+export { DateText, Text, FormDisplay, FormDisplayChildren } from "./text";
 export { LoadingModal } from "./modal";
 export { GoogleMap, Marker } from "./map";
 export { Grid } from "./grid";
 export { TextButton } from "./button";
+export { Row } from "./row";
---
+++ src/components/common/row/Row.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { rowLayoutH, rowLayoutV } from "constants/types";
+
+interface IRow {
+  layoutH?: rowLayoutH;
+  layoutV?: rowLayoutV;
+  style?: any;
+}
+
+class Row extends React.Component<IRow> {
+  public render() {
+    const { children, style } = this.props;
+
+    return (
+      <div style={{ ...styles.container, ...this.getLayout(), ...style }}>
+        {children}
+      </div>
+    );
+  }
+
+  private getLayout = () => {
+    const { layoutH, layoutV } = this.props;
+    const layout: any = {};
+    switch (layoutH) {
+      case "left":
+        layout.justifyContent = "flex-start";
+        break;
+      case "right":
+        layout.justifyContent = "flex-end";
+        break;
+      case "spaceAround":
+        layout.justifyContent = "space-around";
+        break;
+      case "spaceBetween":
+        layout.justifyContent = "space-between";
+        break;
+    }
+    switch (layoutV) {
+      case "center":
+        layout.alignItems = "center";
+        break;
+      case "bottom":
+        layout.alignItems = "flex-end";
+        break;
+    }
+    return layout;
+  };
+}
+const styles = {
+  container: { display: "flex", flexDirection: "row" }
+};
+export default Row;
