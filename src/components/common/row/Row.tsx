3a90517ac902b5998f91e33b00a38886f8529110
annop kongdeedai <mooliveaa@gmail.com>
2020-02-29 18:18:47 +0700

    Update Row.tsx

--- src/components/common/row/Row.tsx
+++ src/components/common/row/Row.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { rowLayoutH, rowLayoutV } from "constants/types";
+import { rowLayoutH, rowLayoutV } from "../../../constants/TYPES";
 
 interface IRow {
   layoutH?: rowLayoutH;
